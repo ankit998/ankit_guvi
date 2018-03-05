95e7fdd15192e2c294313e50e902e47e146cfdb9
ankit agrawal <ankitstudy9@gmail.com>
2018-03-05 19:14:27 +0530

    Update h03.c

--- h03.c
+++ h03.c
@@ -2,7 +2,7 @@
 int main()
 {
 	int n,i;
-	
+	int f=0;
 	scanf("%d",&n);
 	int a[n+1];
 		for(i=0;i<n;i++)
@@ -14,9 +14,11 @@ int main()
 	{
 	if(a[i]==i)
 	{
-	
+	f=1;
 	printf("%d ",i);
 	}
 	}
+	if(f==0)
+	printf("-1");
 return 0;	
 }
